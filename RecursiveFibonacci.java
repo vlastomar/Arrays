91dc87f4d9a0e79eb5f1636c35863ac954f16df9
VSM <vlad24@abv.bg>
2020-07-24 16:34:42 +0300

    Create RecursiveFibonacci.java

---
+++ RecursiveFibonacci.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class RecursiveFibonacci {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int numberFibonacci = Integer.parseInt(scan.nextLine());
+
+        
+    }
+}
