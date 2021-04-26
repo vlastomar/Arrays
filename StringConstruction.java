3c45d093b369b40fc21c99c5fa698ae9e8ec9c75
Vladimir Marinov <vlad24@abv.bg>
2021-04-26 08:58:43 +0300

    Create StringConstruction.java

---
+++ StringConstruction.java
@@ -0,0 +1,7 @@
+public class StringConstruction {
+    public static void main(String[] args) {
+        String s = "abcabc";
+            System.out.println(s.chars().distinct().count());
+
+    }
+}
